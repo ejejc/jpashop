93a57502b13b3a3f26c974772cf0f18900e07e21
eunjis <ejejc@naver.com>
2023-04-19 16:58:33 +0900

    JPA Entity 설계2.

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -11,4 +11,10 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+    protected Address() {} // JPA 스펙 상 기본생성자가 반드시 필요하다.
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+        joinColumns = @JoinColumn(name = "category_id"),
+        inverseJoinColumns = @JoinColumn(name = "item_id")) // 위와 같이 중간 테이블 생성 시, 필드 추가 불가
+    private List<Item> items = new ArrayList<>();
+
+    // TODO: parent 와 child는 잘 이해가 안간다. 기본 원리 듣고 다시 봐보자 !!
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+}
