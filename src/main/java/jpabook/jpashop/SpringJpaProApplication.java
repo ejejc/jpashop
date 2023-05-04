b775f96db53dae1b58c2e870b50c451a4e2f4261
eunjis <ejejc@naver.com>
2023-05-04 18:20:24 +0900

    간단한 주문조회 V1.

--- src/main/java/jpabook/jpashop/SpringJpaProApplication.java
+++ src/main/java/jpabook/jpashop/SpringJpaProApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringJpaProApplication {
@@ -12,6 +14,14 @@ public class SpringJpaProApplication {
         String data = hello.getData();
         System.out.println("data = " + data);
         SpringApplication.run(SpringJpaProApplication.class, args);
+    }
+
+    @Bean
+    Hibernate5Module hibernate5Module() {
+        Hibernate5Module hibernate5Module = new Hibernate5Module();
+        // 좋지 않은 옵션 설정
+        // hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+        return hibernate5Module;
     }
 
 }
---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        for (Order order : all) {
+            // getMember() 까지는 proxy 객체이지만 내부 필드를 꺼내오는 순간 db 쿼리를 날린다.
+            order.getMember().getName(); // Lazy 강제 초기화
+            order.getDelivery().getAddress(); // Lazy 강제 초기화
+        }
+        return all;
+    }
+}
